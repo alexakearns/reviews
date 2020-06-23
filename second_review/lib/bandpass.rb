cabde909ccee8acb8af49ca206a0410da2c8fe39
Alexa <alexakearns@outlook.com>
2020-06-23 16:29:21 +0100

    Add upper limit arg with default - Green

--- second_review/lib/bandpass.rb
+++ second_review/lib/bandpass.rb
@@ -1,16 +1,15 @@
 class Bandpass
 
-  def filter(frequencies, lower_limit = 40)
+  def filter(frequencies, lower_limit = 40, upper_limit = 1000)
     frequencies.map { |frequency| 
 
       if frequency < lower_limit 
         lower_limit
-      elsif frequency > 1000
-        1000
+      elsif frequency > upper_limit
+        upper_limit
       else
         frequency
       end
   }
   end
-
 end
