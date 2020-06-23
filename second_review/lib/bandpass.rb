36c7425735159b9a4369948ae0d95587db667008
Alexa <alexakearns@outlook.com>
2020-06-23 15:44:49 +0100

    Add lower limit value and default - Green

--- second_review/lib/bandpass.rb
+++ second_review/lib/bandpass.rb
@@ -1,10 +1,10 @@
 class Bandpass
 
-  def filter(frequencies)
+  def filter(frequencies, lower_limit = 40)
     frequencies.map { |frequency| 
 
-      if frequency < 40  
-        40
+      if frequency < lower_limit 
+        lower_limit
       else
         frequency
       end
