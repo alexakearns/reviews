f02c7d56e12fd81d0e33658c46afd298a821157c
Alexa <alexakearns@outlook.com>
2020-06-23 16:39:27 +0100

    Raise error if array is empty - Green

--- second_review/lib/bandpass.rb
+++ second_review/lib/bandpass.rb
@@ -1,15 +1,18 @@
 class Bandpass
 
   def filter(frequencies, lower_limit = 40, upper_limit = 1000)
-    frequencies.map { |frequency| 
-
-      if frequency < lower_limit 
-        lower_limit
-      elsif frequency > upper_limit
-        upper_limit
-      else
-        frequency
-      end
-  }
+    if frequencies.length == 0
+      raise 'Error'
+    else 
+      frequencies.map { |frequency| 
+        if frequency < lower_limit 
+          lower_limit
+        elsif frequency > upper_limit
+          upper_limit
+        else
+          frequency
+        end
+      }
+    end
   end
 end
